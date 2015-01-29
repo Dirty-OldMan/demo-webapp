fa5721d290cc90acb0328b6d8c183b3f500bafc6
Tim Brown <tpbrown@gmail.com>
2015-01-28 23:15:26 -0800

    reworked artifacts and reports

--- build.sh
+++ build.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 echo '(pseudo build script.  Pass any parameter to fail.)'
-mkdir -p results
-rm -f results/artifact.txt
-rm -f results/junit.xml
+rm -f results
+mkdir -p results/deployables
+mkdir -p results/reports
+artifact_name="results/deployables/${GO_PIPELINE_NAME}-${GO_PIPELINE_LABEL}.${GO_STAGE_COUNTER}.txt"
 echo $GO_PIPELINE_NAME build $GO_PIPELINE_LABEL triggered
 echo COMPILE..
 echo UNIT TEST...
-echo ${GO_PIPELINE_NAME}-${GO_PIPELINE_LABEL}-${GO_STAGE_COUNTER}>results/artifact.txt
+echo "${GO_PIPELINE_NAME}-${GO_PIPELINE_LABEL}.${GO_STAGE_COUNTER}(${GO_TO_REVISION})">${artifact_name}
 if [ -z "$1" ]
 then
-   cp -f fake-outputs/junit-pass.xml results/junit.xml
+   cp -f fake-outputs/junit-pass.xml results/reports/junit.xml
    exit 0
 fi
-cp -f fake-outputs/junit-fail.xml results/junit.xml
+cp -f fake-outputs/junit-fail.xml results/reports/junit.xml
 exit 1
